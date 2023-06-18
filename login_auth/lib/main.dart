75e761d6f201c7a4ff5222501464cc5d88aee0ea
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-06-18 20:20:05 +0530

    Fire Base Connect

--- login_auth/lib/main.dart
+++ login_auth/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login_auth/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
