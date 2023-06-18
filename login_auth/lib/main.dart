4e810c267dfb6d56d8b4afd3dbbed000bf932438
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-06-18 22:53:03 +0530

    Update main.dart

--- login_auth/lib/main.dart
+++ login_auth/lib/main.dart
@@ -3,7 +3,11 @@ import 'package:login_auth/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
